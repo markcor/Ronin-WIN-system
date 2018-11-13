cd8bb2e8a0248b12128d49e285eb6345e6738ffc
markcor <mcornmesser@mozilla.com>
2018-11-13 10:32:58 -0800

    correction

--- manifests/registry/pause_win_update.pp
+++ manifests/registry/pause_win_update.pp
@@ -1,5 +1,8 @@
 class system::registry::pause_win_update {
 
+	registry_key { 'HKLM\SOFTWARE\Microsoft\Windows\Windows':
+		ensure => present,
+	}
 	registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\Windows\AU':
   	ensure => present,
   	type   => dword,
